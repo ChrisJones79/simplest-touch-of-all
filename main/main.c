408f949ff8eff278e09c971c85a94c100b58528c
ChrisJones79 <christopher.jones.wa@gmail.com>
2023-06-09 08:21:18 -0400

    first commit

---
+++ main/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_err.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_touch.h"
+#include "driver/spi_master.h"
+
+void app_main(void)
+{
+
+}
